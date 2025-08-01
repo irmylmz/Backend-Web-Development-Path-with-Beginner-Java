340601d795e2cd2e684ca60a0ae2f21456e1370c
irmylmz <111319481+irmylmz@users.noreply.github.com>
2025-08-01 18:58:11 +0300

    Program to Find Even Numbers Up to Entered Number

---
+++ Java101_ConditionalStatementsAndCodeBlocks/src/ProgramToFindEvenNumbersUpToEnteredNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ProgramToFindEvenNumbersUpToEnteredNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int k = sc.nextInt();
+		
+		/*
+		for(int i=0; i<=k; i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+			}
+		}
+		*/
+		
+		int i = 0;
+		while(i<=k) {
+			if(i%2 == 0) System.out.println(i);
+			i++;
+		}
+	}
+
+}
